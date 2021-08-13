0fd9fc707f06896383abb9e01180755e686b393c
agb1986 <abrolly1986@gmail.com>
2021-08-13 17:20:44 +0100

    Adding application.yml files & SpringApplication to main files

--- courses/src/main/java/courses/CoursesApplication.java
+++ courses/src/main/java/courses/CoursesApplication.java
@@ -1,7 +1,15 @@
 package courses;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+@EnableDiscoveryClient
 public class CoursesApplication {
     public static void main(String[] args) {
-        System.out.println("@CoursesApplication");
+        SpringApplication.run(CoursesApplication.class, args);
     }
 }
--- users/src/main/java/users/UsersApplication.java
+++ users/src/main/java/users/UsersApplication.java
@@ -1,8 +1,16 @@
 package users;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
 public class UsersApplication {
     
     public static void main(String[] args) {
-        System.out.println("@UsersApplication");
+        SpringApplication.run(UsersApplication.class, args);
     }
 }
